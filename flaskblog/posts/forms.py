73406feb90455356fc77b255f25812ee02eaa5f5
MyroslavG <realmyros@gmail.com>
2023-08-02 15:23:06 -0400

    reqs file updated

--- flaskblog/posts/forms.py
+++ flaskblog/posts/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])                  
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
+    title = StringField('TITLE', validators=[DataRequired()])                  
+    content = TextAreaField('CONTENT', validators=[DataRequired()])
+    submit = SubmitField('POST')
