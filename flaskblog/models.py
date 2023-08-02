ab95e824add76a89610ea801cce3dbde50af775c
MyroslavG <realmyros@gmail.com>
2023-08-02 15:15:13 -0400

    reqs file updated

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -14,7 +14,7 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable = False, default='default.jpg')  
     password = db.Column(db.String(60), nullable = False)
     posts = db.relationship('Post', backref = 'author', lazy = True)
-    birthday = db.Column(db.String(20), nullable=False)
+    #birthday = db.Column(db.String(20))
 
     def get_reset_token(self, expires_sec=1800):
         s = Serializer(app.config['SECRET_KEY'], expires_sec)
